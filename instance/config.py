d40ebb512da191874177c5e039966ec56e443573
Stéphane FEUGA OSHIMA <sfeuga@member.fsf.org>
2021-01-21 13:00:05 +0900

    [ADD] Minimal Flask App

--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,32 @@
 
+import os
+
+class Config(object):
+    """Defaults Configurations"""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    DEBUG = True
+
+class StagingConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
